799c397ac96129f183d9428d0696ca7eb30c963d
JaeseungJung <86044304+JaeseungJung@users.noreply.github.com>
2021-06-28 15:39:10 +0900

    [UPDATE]

--- scripts/common.js
+++ scripts/common.js
@@ -23,3 +23,38 @@ function getAgeGroup(user, keyAge) {
 
 
 console.log(getAgeGroup());
+
+
+
+function getRandomInteger(min, max) {
+    return Math.floor(Math.random()) * (max - min + 1) + min;
+}
+
+console.log(getRandomInteger(11, 20))
+
+
+
+
+
+
+// 강사님 해답02
+function rspPlayer02(userRsp) {
+    const rsp = ["가위", "바위", "보"];
+    const playerRsp = rsp[getRandomInteger(0, 2)];
+    const winValue = {
+        "가위": "보",
+        "바위": "가위",
+        "보": "바위"
+    };
+
+
+    return {
+        "userRsp": userRsp,
+        "playerRsp": playerRsp,
+        "winner": (userRsp == playerRsp) ? 0 : (winValue[userRsp] == playerRsp) ? 1 : -1
+    }
+}
+
+
+
+console.table(rspPlayer02("가위"));
